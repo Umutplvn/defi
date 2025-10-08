9f5d6d22083e76a4cc9a6204a836114d9d886f3c
umutplvn <umutpehlivan2078@gmail.com>
2025-10-08 21:25:32 +0100

    up

--- src/components/Section3.jsx
+++ src/components/Section3.jsx
@@ -105,8 +105,8 @@ const Section3 = () => {
               width: "100%",
               display: "flex",
               justifyContent: "center",
-              alignItems: "center",
-              height: "100%"
+              height: "100%",
+              mt:{xs:"0",md:"2rem"}
                         }}
           >
             <img
